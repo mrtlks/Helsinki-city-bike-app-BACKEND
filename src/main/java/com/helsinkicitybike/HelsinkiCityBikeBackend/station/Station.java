2ea11d5edc23f898bbbaa8e9a47cb2a91f1f20d6
mrtlks <61003410+mrtlks@users.noreply.github.com>
2023-02-05 17:56:58 +0200

    Update Station.java

--- src/main/java/com/helsinkicitybike/HelsinkiCityBikeBackend/station/Station.java
+++ src/main/java/com/helsinkicitybike/HelsinkiCityBikeBackend/station/Station.java
@@ -12,7 +12,7 @@ import javax.persistence.Id;
 public class Station {
 
 	@Id
-	//@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
 	private int fid;
 	private String name;
